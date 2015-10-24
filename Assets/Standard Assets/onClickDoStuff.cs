de47259976c82350945f030289aaf3267ace4236
APerno3003 <APerno3003@gmail.com>
2015-10-24 16:05:36 -0400

    we can now click and do stuff

---
+++ Assets/Standard Assets/onClickDoStuff.cs
@@ -0,0 +1,44 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class onClickDoStuff : MonoBehaviour {
+
+    private bool isColliding;
+    private Collider currentCollider;
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (isColliding == true)
+        {
+            if (currentCollider.gameObject.name == "Drawer 1" && Input.GetMouseButtonDown(0))
+            {
+                //currentCollider.gameObject.PlayAnimation("Open Drawer 1");
+                Debug.Log("DRAWER !!!!!!");
+                currentCollider = null;
+                isColliding = false;
+            }
+        }
+	}
+
+    void OnTriggerEnter(Collider theCollider)
+    {
+        Debug.Log("OnTriggerEnter");
+        isColliding = true;
+        currentCollider = theCollider;
+
+    }
+
+    void OnTriggerExit(Collider theCollider)
+    {
+        Debug.Log("OnTriggerExit");
+        isColliding = false;
+        currentCollider = null;
+    }
+
+}
+
+
