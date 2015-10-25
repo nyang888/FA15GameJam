c482fc9d61847728512e6cc8f828d28de6567d31
APerno3003 <APerno3003@gmail.com>
2015-10-25 13:39:51 -0400

    you can now pick up the phone
    Also added new quest

--- Assets/Standard Assets/QuestTracker.cs
+++ Assets/Standard Assets/QuestTracker.cs
@@ -1,21 +1,28 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class QuestTracker : MonoBehaviour {
+public class QuestTracker : MonoBehaviour
+{
     public static bool internet = false;
     public static bool sleep = false;
     public static bool shit = false;
+    public static bool phone = false;
+    public static bool study = false;
     public string questCompleted = " - Completed";
     public string questOne = "Internet";
     public string questTwo = "Sleep";
     public string questThree = "Shit";
+    public string questFour = "Pick Up Phone";
+    public string questFive = "Study";
     public string questOneOriginalText = "Internet";
     public string questTwoOriginalText = "Sleep";
     public string questThreeOriginalText = "Shit";
-    
+    public string questFourOriginalText = "Pick Up Phone";
+    public string questFiveOriginalText = "Study";
 
     // Update is called once per frame
-	void Update () {
+    void Update()
+    {
         if (internet == true && questOne != questOneOriginalText + questCompleted)
         {
             questOne = questOne + questCompleted;
@@ -28,11 +35,20 @@ public class QuestTracker : MonoBehaviour {
         {
             questThree = questThree + questCompleted;
         }
-	}
+        else if (phone == true && questFour != questFourOriginalText + questCompleted)
+        {
+            questFour = questFour + questCompleted;
+
+        }
+        else if (study == true && questFive != questFiveOriginalText + questCompleted)
+        {
+            questFive = questFive + questCompleted;
+        }
+    }
 
     void OnGUI()
     {
-        GUI.Box(new Rect(10, 50, 200, 500), questOne +"\n" + questTwo + "\n" + questThree);
+        GUI.Box(new Rect(10, 50, 200, 500), questOne + "\n" + questTwo + "\n" + questThree + "\n" + questFour + "\n" + questFive);
     }
-   
+
 }
