1614ad48110c894f1e10febf79cb1d583b4b17c0
APerno3003 <APerno3003@gmail.com>
2015-10-24 20:57:58 -0400

    You can now complete quests

--- Assets/Standard Assets/QuestTracker.cs
+++ Assets/Standard Assets/QuestTracker.cs
@@ -5,14 +5,34 @@ public class QuestTracker : MonoBehaviour {
     public static bool internet = false;
     public static bool sleep = false;
     public static bool shit = false;
-	
-	// Update is called once per frame
+    public string questCompleted = " - Completed";
+    public string questOne = "Internet";
+    public string questTwo = "Sleep";
+    public string questThree = "Shit";
+    public string questOneOriginalText = "Internet";
+    public string questTwoOriginalText = "Sleep";
+    public string questThreeOriginalText = "Shit";
+    
+
+    // Update is called once per frame
 	void Update () {
-	
+        if (internet == true && questOne != questOneOriginalText + questCompleted)
+        {
+            questOne = questOne + questCompleted;
+        }
+        else if (sleep == true && questTwo != questTwoOriginalText + questCompleted)
+        {
+            questTwo = questTwo + questCompleted;
+        }
+        else if (shit == true && questThree != questThreeOriginalText + questCompleted)
+        {
+            questThree = questThree + questCompleted;
+        }
 	}
 
     void OnGUI()
     {
-        GUI.Box(new Rect(10, 50, 200, 500), "Internet ("+" "+")\nSleep ("+" "+")\nShit ("+" "+")");
+        GUI.Box(new Rect(10, 50, 200, 500), questOne +"\n" + questTwo + "\n" + questThree);
     }
+   
 }
